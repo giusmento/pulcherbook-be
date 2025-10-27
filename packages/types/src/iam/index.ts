ed8a57ded41d3c532e425e4d27f8624ae91749aa
giuseppemento <giuseppe.mento@jakala.com>
2025-10-27 13:46:53 +0100

    exp

--- packages/types/src/iam/index.ts
+++ packages/types/src/iam/index.ts
@@ -1,7 +1,7 @@
 // Shared types for PulcherBook IAM services
 import { services } from "@giusmento/mangojs-core";
-import * as mjcore from "@giusmento/mangojs-core";
+import * as mjenums from "@giusmento/mangojs-core/types/enums";
 
 export import iam = services.iam_server.types;
 
-export import enums = mjcore.Enums;
+export import enums = mjenums;
