09e11e05b0d27a58eaafaa5a240e9f4458876ec8
giuseppemento <giuseppe.mento@jakala.com>
2025-10-27 15:01:17 +0100

    remove enums

--- packages/types/src/iam/index.ts
+++ packages/types/src/iam/index.ts
@@ -1,7 +1,4 @@
 // Shared types for PulcherBook IAM services
 import { services } from "@giusmento/mangojs-core";
-import * as mjenums from "@giusmento/mangojs-core/types/enums";
 
 export import iam = services.iam_server.types;
-
-export import enums = mjenums;
